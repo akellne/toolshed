8d284041279f60a1daae6f426ac5cf96835688b3
Ansgar Kellner <kellner@cs.uni-goettingen.de>
2013-05-24 14:58:42 +0200

    * updated base plugin to store cache data as utf-8 * added fastfood plugin (currently only king of the month)

--- plugins/base.py
+++ plugins/base.py
@@ -3,6 +3,7 @@ import sys
 import json
 import logging
 import datetime
+import codecs
 
 
 #setup logger for plugins
@@ -23,23 +24,23 @@ class Plugin(object):
     AUTHOR  = ""
     VERSION = (0, 0, 0)
     ENABLED = False
-    
+
     def __init__(self, ircbot, cache_time=None):
         #setup plugin logger
         self.log = logging.getLogger(
             "plugin '%s (%s)':" % (self.NAME, self.get_version())
         )
-        
+
         self.log.debug("Init with cache_time '%s'" % cache_time)
-        
+
         #ircbot that is used for information exchange
         self.ircbot = ircbot
-        
+
         #build cache file name based on plugin name
         self.cache_file     = os.path.join(
             CACHE_DIR, "%s.dat" % self.NAME.lower().replace(" ", "_")
         )
-        
+
         #if set to None, no caching => otherwise use datetime.timedelta
         self.cache_time = cache_time
 
@@ -102,7 +103,7 @@ class Plugin(object):
                     #cache time is not expired => load cached data
                     self.log.debug("Loading data from cache")
                     reload_data = False
-                    with open(self.cache_file, "r") as f:
+                    with codecs.open(self.cache_file, "r", "utf-8") as f:
                         data = json.load(f)
                 else:
                     self.log.debug(
@@ -117,7 +118,7 @@ class Plugin(object):
                 "cachetime: '%s' is None or file '%s' does not " \
                 "exist" % (self.cache_time, self.cache_file)
             )
-            
+
         return (reload_data, data)
 
 
@@ -126,8 +127,8 @@ class Plugin(object):
         save given data to the cache
         """
         self.log.debug("Storing data to cache: %s" % str(data))
-        with open(self.cache_file, "w") as f:
-            f.write(json.dumps(data))        
+        with codecs.open(self.cache_file, "w", "utf-8" ) as f:
+            f.write(json.dumps(data))
 
 
 
---
+++ plugins/fastfood.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import sys
+import datetime
+import lxml.html
+import json
+
+from base import Plugin
+
+#URL to burgerking
+URL = "http://www.burgerking.de"
+
+
+class FastFood(Plugin):
+    """
+    class to parse some fastfood webpages
+    """
+    NAME    = "Fastfood"
+    AUTHOR  = "kellner@cs.uni-goettingen.de"
+    VERSION = (0, 0, 1)
+    ENABLED = True
+
+    def __init__(self, ircbot, cache_time=datetime.timedelta(days=1)):
+        Plugin.__init__(self, ircbot, cache_time)
+
+
+    def on_privmsg(self, msg, *params):
+        Plugin.on_privmsg(self, msg, *params)
+
+        if msg == "!fastfood":
+            #get data from cache
+            self.ircbot.switch_personality(nick="bkismyway")
+
+            reload_data, message = self.load_cache()
+            if reload_data:
+                #reload the data, if too old
+                message = self._get_king_of_the_month()
+                self.save_cache(data=message)
+            else:
+                message = message.encode("utf-8")
+
+            #finally, send the message with the
+            self.ircbot.privmsg(params[0], message)
+
+            self.ircbot.reset_personality()
+
+
+    def _get_king_of_the_month(self):
+        """
+        load king of the month from burgerking webpage
+        """
+        try:
+            #load url and parse it with html parser
+            root = lxml.html.parse(URL)
+
+            #get relevant part
+            el = root.xpath(
+                "//h2[text()='King des Monats']/following-sibling::div[@class='info']/p"
+            )
+
+            res = re.compile("Der (.*?) -" ).search(el[0].text)
+            if res:
+                text = "--- King of the Month: '%s' ---\n%s" % (
+                    res.group(1),  el[0].text
+                )
+            else:
+                text = "--- King of the Month ---\n%s" % el[0].text
+
+        except:
+            text = "Sorry, burgerking parser is broken. Cannot get " \
+                   "the King of the Month"
+
+        return text.encode("utf-8")
+
+
