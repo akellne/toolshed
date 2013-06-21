79a08dd713ffd5bdaf0eb8260d44b4d3d663ee19
Droopy <droopy@mlsec.org>
2013-06-21 19:50:42 +0200

    sync

--- plugins/durchsage.py
+++ plugins/durchsage.py
@@ -21,11 +21,11 @@ class Static(Plugin):
 
     def __init__(
         self, ircbot, cache_time=None, 
-        random_message=[2 * 60, 8 * 60] # between 2 h and 8 h
+        random_message=[3 * 60, 9 * 60] # between 3 h and 9 h
     ):
         Plugin.__init__(self, ircbot, cache_time, random_message)
 
-    def get_train():
+    def get_train(self):
         r = random.random()
         if r < 0.33:
             return "RE %5d" % random.randint(10000,100000)
@@ -34,29 +34,30 @@ class Static(Plugin):
         else:
             return "ICE %3d" % random.randint(100,1000)
 
-    def get_delay():
+    def get_delay(self):
         mins = [5, 10, 15, 20, 30, 40, 45, 60, 90, 120]
         return random.choice(mins)
 
-    def get_cities():
+    def get_cities(self):
         cities = open('plugins/durchsage_s.txt').read().splitlines()
         y = x = random.choice(cities).strip()
         while y == x:
             y = random.choice(cities).strip()
         return (x,y)
 
-    def get_time():
+    def get_time(self):
         return '%d:%.2d' % (random.randint(0,24), random.randint(0,60))
 
-    def get_reason():
+    def get_reason(self):
         reasons = open('plugins/durchsage_g.txt').read().splitlines()
         return random.choice(reasons).strip()
     
-    def durchsage():
-        (x,y) = get_cities()
+    def durchsage(self):
+        (x,y) = self.get_cities()
         text = "Information zu %s nach %s über %s, Abfahrt %s, " \
                "heute circa %d Minuten später. %s." % \
-               (get_train(), x, y, get_time(), get_delay(), get_reason())
+               (self.get_train(), x, y, self.get_time(), \
+                self.get_delay(), self.get_reason())
         return text
 
     def on_random_message(self):
@@ -66,5 +67,8 @@ class Static(Plugin):
     def on_privmsg(self, msg, *params):
         Plugin.on_privmsg(self, msg, *params)
 
-        if msg == "!durchsage" or "bahn" in msg:
-            self.ircbot.privmsg(params[0], durchsage())
+        if msg == "!durchsage":
+            self.ircbot.privmsg(params[0], self.durchsage())
+            
+        if "bahn" in msg and random.random() < 0.5:
+            self.ircbot.privmsg(params[0], self.durchsage())
